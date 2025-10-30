31c12da2a063745300064ad369217be8ae7e849a
click-clack <35391305+jwllzp@users.noreply.github.com>
2025-10-29 22:32:16 -0600

    Add neomodern.nvim plugin configuration

---
+++ lua/plugins/neomodern.lua
@@ -0,0 +1,51 @@
+return {
+	"cdmill/neomodern.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("neomodern").setup({
+			theme = "iceclimber",
+			variant = "dark",
+			alt_bg = true,
+			colored_docstrings = false,
+			cursorline_gutter = true,
+			dark_gutter = false,
+			favor_treesitter_hl = false,
+			plain_float = false,
+			show_eob = true,
+			term_colors = true,
+			toggle_variant_key = nil,
+			transparent = false,
+			diagnostics = {
+				darker = true,
+				undercurl = true,
+				background = true,
+			},
+			code_style = {
+				comments = "none",
+				conditionals = "none",
+				functions = "none",
+				keywords = "bold",
+				headings = "bold", -- Markdown headings
+				operators = "none",
+				keyword_return = "bold",
+				strings = "none",
+				variables = "none",
+			},
+			highlights = {
+				FloatBorder = { fg = "#555568", bg = "none" },
+				NormalFloat = { bg = "#111113", fg = "##bbbac1" },
+				TelescopeMatching = { fg = "#e67e80" },
+				TelescopeSelection = { bg = "#2a2a31" },
+				["@keyword.conditional"] = { fg = "#8a88db", fmt = "bold" },
+				["@keyword.operator"] = { fg = "#8a88db", fmt = "bold" },
+				["@keyword.exception"] = { fg = "#e67e80", fmt = "bold" },
+				["@boolean"] = { fg = "#cfa18c", fmt = "bold" },
+				["@lsp.type.macro"] = { fg = "#cfa18c", fmt = "none" },
+				["@lsp.type.lifetime"] = { fg = "#a8a6de", fmt = "none" },
+			},
+		})
+		require("neomodern").load()
+	end,
+}
+:
